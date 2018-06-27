75b9a9cfe91c3cea654f0c3155579c8d16bdfbc9
shaygyawali <shaygyawali@gmail.com>
2018-06-27 19:51:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+ 
+  get '/' do
+    erb :index
+  end
+  
+  get '/understand' do
+    erb :understand
+  end
+  
+  get '/explore' do
+    erb :explore
+  end
 end
